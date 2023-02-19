a33c726ede64f892e5966665f1aeb602815f3a74
Lester <32542078+lhes23@users.noreply.github.com>
2023-02-19 15:37:01 +0800

    add id

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,30 +1,31 @@
+import Link from "next/link"
 import React from "react"
 
 type Props = {
+  id: string
   name: string
   img: string
   price: string
   description: string
 }
 
-const ProductCard = ({ name, img, price, description }: Props) => {
+const ProductCard = ({ id, name, img, price, description }: Props) => {
   return (
     <>
-      <div className="card w-96 glass shadow-xl transform duration-200 ease-in-out hover:scale-105">
-        <figure>
-          <img src={img} alt={name} />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <p>{description}</p>
-          <div className="card-actions">
-            <div className="justify-start">{price}</div>
-            <div className="justify-end">
-              <button className="btn btn-primary">Buy Now</button>
+      <Link href={`/products/${id}`} passHref>
+        <div className="card w-96 glass hover:shadow-xl transform duration-200 ease-in-out hover:scale-105">
+          <figure>
+            <img src={img} alt={name} />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{name}</h2>
+            <p className="line-clamp-3">{description}</p>
+            <div className="card-actions">
+              <div className="justify-end">{price}</div>
             </div>
           </div>
         </div>
-      </div>
+      </Link>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import ProductCard from "../components/ProductCard"
 const Home: NextPage = () => {
   const meats = [
     {
+      id: "1",
       name: "A5 MIYAZAKI JAPANESE WAGYU NY STRIP (14-16oz. Each)",
       price: "119.95",
       img: "https://www.subicmeatsandseafoods.com/wp-content/uploads/2022/07/img_5015.jpeg",
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
         "Japan Premium Beef carries imported Wagyu Beef from the Miyazaki prefecture of Japan. This is called ” MiyazakiGyu” and it’s Rated “A5” by the Japanese Government beef grading system."
     },
     {
+      id: "2",
       name: "A5 MIYAZAKI JAPANESE WAGYU FILET MIGNON (7-8oz. Each)",
       price: "109.95",
       img: "https://www.subicmeatsandseafoods.com/wp-content/uploads/2022/07/img_5017.jpeg",
@@ -20,6 +22,7 @@ const Home: NextPage = () => {
         "All Japanese A5 Wagyu arrives with Certificate of Authenticity that includes the nose print of the cow."
     },
     {
+      id: "3",
       name: "A5 MIYAZAKI JAPANESE WAGYU FILET MIGNON (7-8oz. Each)",
       price: "109.95",
       img: "https://www.subicmeatsandseafoods.com/wp-content/uploads/2022/07/img_5017.jpeg",
@@ -27,6 +30,7 @@ const Home: NextPage = () => {
         "All Japanese A5 Wagyu arrives with Certificate of Authenticity that includes the nose print of the cow."
     },
     {
+      id: "4",
       name: "A5 MIYAZAKI JAPANESE WAGYU FILET MIGNON (7-8oz. Each)",
       price: "109.95",
       img: "https://www.subicmeatsandseafoods.com/wp-content/uploads/2022/07/img_5017.jpeg",
@@ -35,11 +39,12 @@ const Home: NextPage = () => {
     }
   ]
   return (
-    <div className="bg-gradient-to-r from-orange-200 to-white">
+    <div className="bg-gradient-to-r from-slate-200 to-slate-100">
       <div className="py-12 max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-16 gap-y-8">
-        {meats.map((meat, i) => (
+        {meats.map((meat) => (
           <ProductCard
-            key={i}
+            key={meat.id}
+            id={meat.id}
             name={meat.name}
             img={meat.img}
             price={meat.price}
